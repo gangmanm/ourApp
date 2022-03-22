d632ff5923df90d282c4ed2643c5e7df870801ce
gangmanm <97601109+gangmanm@users.noreply.github.com>
2022-03-23 01:11:14 +0900

    ㄷㄹㄷ

--- app/src/main/java/com/example/application/CustomDialog.java
+++ app/src/main/java/com/example/application/CustomDialog.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+//ㅇㄹㄹㄷㄹ
 class CustomDialog extends Dialog implements View.OnClickListener{
 
     private Button positiveButton;
